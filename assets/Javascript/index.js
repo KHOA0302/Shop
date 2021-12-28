052ce8a14c205998a4a9f6e2027b2e4f04b4c53d
Your Name <you@example.com>
2021-12-28 21:08:25 +0700

    shop

--- assets/Javascript/index.js
+++ assets/Javascript/index.js
@@ -6,3 +6,7 @@ const notifyItems = $$('.header__notify-item')
 notifyItems.forEach((notifyItem, index) => notifyItem.onclick = function() {
     this.classList.add('header__notify-item--viewed')
 })  
+
+const signInBtn = $('.header__navbar-list:last-child > .header__navbar-item:nth-child(3)')
+const signUpBtn = $('.header__navbar-list:last-child > .header__navbar-item:nth-child(4)')
+
